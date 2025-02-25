3f19298b3d41af8733f48c198f02ab3daf301435
Coca <coca16622@gmail.com>
2025-02-25 10:41:59 +0000

    Switch to just manual pinning

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
 {pkgs ? import <nixpkgs> {}}: let
-  pins = import ./npins;
   inherit (pkgs) lib;
-  craneLib = import pins.crane {inherit pkgs;};
+  crane = pkgs.fetchzip {
+    url = "https://github.com/ipetkov/crane/archive/19de14aaeb869287647d9461cbd389187d8ecdb7.tar.gz";
+    sha256 = "0a8rbd0nc7iad7a5c1ask2kc7zhcigvhrrsf8zjidfal6d9352y7";
+  };
+  craneLib = import crane {inherit pkgs;};
 
   unfilteredRoot = ./.;
   src = lib.fileset.toSource {
@@ -26,4 +29,5 @@ in
   // {
     # Allow for reuse of previous dependency builds
     cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+    meta.mainProgram = "oxibot";
   }

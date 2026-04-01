950963b17b547abe75160eda899ed8ba1e6307d9
Coca <me@coca.codes>
2026-04-01 18:21:46 +0100

    Update crane

--- default.nix
+++ default.nix
@@ -1,8 +1,10 @@
 {pkgs ? import <nixpkgs> {}}: let
   inherit (pkgs) lib;
-  crane = pkgs.fetchzip {
-    url = "https://github.com/ipetkov/crane/archive/19de14aaeb869287647d9461cbd389187d8ecdb7.tar.gz";
-    sha256 = "0a8rbd0nc7iad7a5c1ask2kc7zhcigvhrrsf8zjidfal6d9352y7";
+  crane = pkgs.fetchFromGitHub {
+    owner = "ipetkov";
+    repo = "crane";
+    rev = "v0.23.2";
+    hash = "sha256-BEp040fMklOW2+ttpNUKivlE0hzpv0AAP+0kG47uVCk=";
   };
   craneLib = import crane {inherit pkgs;};
 
